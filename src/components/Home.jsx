664cddd50d8d93645763f7901465fab77666c2d0
Sultana Razia <sultanarazia483@gmail.com>
2024-07-15 10:55:56 +0600

    Create User Context For Shared Authentication

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,12 @@
+import { useContext } from "react";
+import { AuthContext } from "../providers/AuthProvider";
 
 
 const Home = () => {
+    const authInfo = useContext(AuthContext)
     return (
         <div>
-            <h2 className="text-3xl">This is Home</h2>
+            <h2 className="text-3xl">This is Home: {authInfo.name}</h2>
         </div>
     );
 };
