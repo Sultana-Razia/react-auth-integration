bf018c1c0f472f7547b28e97051a2ae762c9227b
Sultana Razia <sultanarazia483@gmail.com>
2024-07-15 16:46:39 +0600

    Add Sign In User Using Context API

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,6 +1,6 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import PropTypes from 'prop-types';
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
 import auth from "../firebase/firebase.config";
 
 
@@ -13,7 +13,24 @@ const AuthProvider = ({ children }) => {
         return createUserWithEmailAndPassword(auth, email, password)
     }
 
-    const authInfo = { user, createUser }
+    const signInUser = (email, password) => {
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    useEffect(() => {
+        const unSubscribe = onAuthStateChanged(auth, currentUser => {
+            if (currentUser) {
+                setUser(currentUser);
+                console.log(currentUser);
+            }
+        });
+
+        return () => {
+            unSubscribe();
+        }
+    }, [])
+
+    const authInfo = { user, createUser, signInUser }
 
     return (
         <AuthContext.Provider value={authInfo}>
